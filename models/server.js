42b4f05a021736b0cf80935cbd821390cc473031
xansiety <ferando543@outlook.com>
2022-10-31 19:42:05 -0600

    express-rate-limit: Se implementa rate limit en peticiones a API

--- models/server.js
+++ models/server.js
@@ -1,40 +1,59 @@
-import express from "express"
-import cors from "cors"
-import { router } from "../routes/user.js"
+import express from "express";
+import rateLimit from "express-rate-limit";
+import cors from "cors";
+import { router } from "../routes/user.js";
 class Server {
   constructor() {
-    this.app = express() //creamos la aplicación de express
-    this.port = process.env.PORT
-    this.usuariosPath = "/api/usuarios"
+    this.app = express(); //creamos la aplicación de express
+    this.port = process.env.PORT;
+    this.usuariosPath = "/api/usuarios";
 
     // Middleware
-    this.Middleware()
+    this.Middleware();
 
     //Rutas de mi aplicación
-    this.routes()
+    this.routes();
   }
 
   Middleware() {
     //CORS
-    this.app.use(cors())
+    this.app.use(cors());
+
+    // Rate limit
+
+    this.app.use(
+      rateLimit({
+        windowMs: 60000, // 15 minutes
+        max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: true, // Disable the `X-RateLimit-*` headers
+        statusCode: 200,
+        message: {
+          status: 429, // optional, of course
+          limiter: true,
+          type: "error",
+          message: "Maximun exceded request calling API ",
+        },
+      })
+    );
 
     // parseo y lectura del body
-    this.app.use(express.json())
+    this.app.use(express.json());
 
     // Directorio publico
-    this.app.use(express.static("public"))
+    this.app.use(express.static("public"));
   }
 
   routes() {
     //rutas separadas
-    this.app.use(this.usuariosPath, router)
+    this.app.use(this.usuariosPath, router);
   }
 
   listen() {
     this.app.listen(this.port, () => {
-      console.log(`Example app listening on port ${this.port}`)
-    })
+      console.log(`Example app listening on port ${this.port}`);
+    });
   }
 }
 
-export default Server
+export default Server;
